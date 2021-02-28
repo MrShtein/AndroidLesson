1b5be8924e6219f6f18d951251d4a5c04c15706a
Mr.Shtein <mr.shtein61@yandex.ru>
2021-02-28 14:06:57 +0300

    Completed lesson 21

---
+++ p0211twoactivites/src/main/java/ru/startandroid/p0211twoactivites/MainActivity.java
@@ -0,0 +1,35 @@
+package ru.startandroid.p0211twoactivites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnActTwo;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnActTwo = (Button) findViewById(R.id.btnActTwo);
+        btnActTwo.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnActTwo:
+                Intent intent = new Intent(this, Activity2.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+    }
+}
